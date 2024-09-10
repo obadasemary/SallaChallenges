ca44fb7730c1cae8efe60eb29399f02971a07f96
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-09-11 01:16:40 +0300

    Initial Commit

---
+++ SallaChallenges/SallaChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SallaChallengesApp.swift
+//  SallaChallenges
+//
+//  Created by Abdelrahman Mohamed on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SallaChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
